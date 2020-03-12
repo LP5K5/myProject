b6f6d6f56862e511e8236bf7d03d30064c0d37a2
LP5K5 <lakshmiprasanna.9580@gmail.com>
2020-03-12 15:40:17 +0530

    Updates

---
+++ precache-manifest.f7bd72f9e30f3adee0c2dc411d771484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555eb055a990c2ba624fce174b333a4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbc2a82fc8b33127a599",
+    "url": "/static/css/main.2f6c3090.chunk.css"
+  },
+  {
+    "revision": "4dd69855f007a1fb40cc",
+    "url": "/static/js/2.e393ae49.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e393ae49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc2a82fc8b33127a599",
+    "url": "/static/js/main.04b89d12.chunk.js"
+  },
+  {
+    "revision": "9470aa76c591bc631d55",
+    "url": "/static/js/runtime-main.3c10d325.js"
+  },
+  {
+    "revision": "efe01b90316cc1eca182c240aa38b1f8",
+    "url": "/static/media/boy.efe01b90.svg"
+  }
+]);
